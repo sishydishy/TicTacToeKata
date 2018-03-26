046a36963bc90ad1b92b05365715a79cf11a0ae6
Saish Dharvotkar <saish.dharvotkar@myob.com>
2018-03-26 14:38:40 +1300

    Initial commit

---
+++ TicTacToe/TicTacToeKata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicTacToeKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
